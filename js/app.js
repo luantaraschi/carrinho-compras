5db4a1fd2a0a1b1786797a4b7e064255b9dce27a
luantaraschi <luantaraschi@gmail.com>
2024-10-09 20:56:29 -0300

    produtos podem ser adicionados ao carrinho

--- js/app.js
+++ js/app.js
@@ -2,8 +2,12 @@ function adicionar() {
     let produto = document.getElementById('produto').value; //pegando  o valor do input produto
     let nomeProduto = produto.split('-')[0]; //separando o nome do produto(o que estava antes do '-') 
     let valorUnitario = produto.split('R$')[1]; //pegando o valor unitario do produto (o que estava após o R$)
-    let quantidade = document.getElementById('quantidade'); //pegando  o valor do input quantidade  
-    let preco =  quantidade.value * valorUnitario; //calculando o preco total
+    let quantidade = document.getElementById('quantidade').value; //pegando  o valor do input quantidade  
+    let preco =  quantidade * valorUnitario; //calculando o preco total
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`; // mudando o conteúdo do carrinho
 
 }
 
