b6d122ec0885808339718e08eec6bcbf21d96a0f
luantaraschi <luantaraschi@gmail.com>
2024-10-09 21:06:45 -0300

    botao limpar funcional e projeto finalizado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let totalGeral = 0;
+
 function adicionar() { 
     let produto = document.getElementById('produto').value; //pegando  o valor do input produto
     let nomeProduto = produto.split('-')[0]; //separando o nome do produto(o que estava antes do '-') 
@@ -8,16 +10,22 @@ function adicionar() {
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`; // mudando o conteúdo do carrinho
-
+        totalGeral += preco; //somando o preco total ao total geral
+        let campoTotal = document.getElementById('valor-total');
+        campoTotal.textContent =  `R$${totalGeral}`; //atualizando o valor total no campo
+        document.getElementById('quantidade').value = 0; //zerando o campo quantidade ao clique
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0,00';
 }
 
 
 //roteiro:
 //recuperar valores de nome do produto, quantidade e valor ✓
 //calcular preco ✓
-//adicionar ao carrinho 
-//atualizar valor total da compra 
+//adicionar ao carrinho ✓
+//atualizar valor total da compra ✓
+//adicionar funcionalidade ao botao limpar ✓
